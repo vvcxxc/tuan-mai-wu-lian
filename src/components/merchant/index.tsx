007b680b610d8b570e7e5bd65f3ba3d79d87ab72
domen <mixiaofeng1996@gmail.com>
2019-05-19 14:31:57 +0800

    feat: 首页

---
+++ src/components/merchant/index.tsx
@@ -0,0 +1,12 @@
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text, Button } from "@tarojs/components";
+import "index.styl";
+
+/**商店 ITEM 组件 */
+export default class Merchant extends Component {
+  state = {};
+
+  render() {
+    return <View className="merchant" />;
+  }
+}
