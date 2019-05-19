ab8aa8849e6c9894e445f45c193ce232d0dc06a2
长安 <627026232@qq.com>
2019-05-19 16:13:47 +0800

    礼品领取

--- src/business-pages/gift/index.tsx
+++ src/business-pages/gift/index.tsx
@@ -2,6 +2,7 @@ import Taro, { Component } from "@tarojs/taro";
 import { View, ScrollView } from "@tarojs/components";
 import { AtIcon } from "taro-ui";
 import GiftItem from "./components/gift-item";
+import GiftItemDetails from "./components/gift-item-details";
 import "./index.styl";
 
 export default class Gift extends Component {
@@ -10,30 +11,36 @@ export default class Gift extends Component {
   };
 
   state = {
-    giftList: []
+    giftList: [],
+    status: true,
+    orderid: 0
   };
 
   componentWillMount() {
     this.setState({ giftList: [1, 1, 1, 1, 1, 1, 1, 1] });
   }
   componentDidMount() {}
+
   render() {
     return (
-      <ScrollView scrollY className="gift">
-        {this.state.giftList.map((_) => (
-          <View className="gift-item">
-            <View className="head flex center">
-              <AtIcon value="home" color="#666" />
-              <View className="title item">杨大富的五金店</View>
-              <AtIcon value="chevron-right" color="#999" />
+      <View>
+        <ScrollView scrollY className="gift">
+          {this.state.giftList.map((_) => (
+            <View className="gift-item">
+              <View className="head flex center">
+                <AtIcon value="home" color="#666" />
+                <View className="title item">杨大富的五金店</View>
+                <AtIcon value="chevron-right" color="#999" />
+              </View>
+              <View className="body">
+                <GiftItem />
+                <GiftItem />
+              </View>
             </View>
-            <View className="body">
-              <GiftItem />
-              <GiftItem />
-            </View>
-          </View>
-        ))}
-      </ScrollView>
+          ))}
+        </ScrollView>
+        <GiftItemDetails status={this.state.status} orderid={this.state.orderid} />
+      </View>
     );
   }
 }
