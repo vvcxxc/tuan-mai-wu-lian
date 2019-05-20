cf625f16506c6684b2e49dc6558e24fd0d6738da
domen <mixiaofeng1996@gmail.com>
2019-05-20 14:28:51 +0800

    feat: 优化优惠券组件样式

--- src/components/cash-coupon/index.tsx
+++ src/components/cash-coupon/index.tsx
@@ -1,5 +1,7 @@
 import Taro, { Component, ComponentOptions } from "@tarojs/taro";
-import { View, Text } from "@tarojs/components";
+import { View, Text, Image } from "@tarojs/components";
+
+import secondaryActiveBg from "./secondary-avitve-bg.png";
 
 import "./index.styl";
 
@@ -18,45 +20,30 @@ export default class CashCoupon extends Component<Props> {
 
   render() {
     return (
-      <View className="cash-coupon flex active">
-        <View className="secondary flex center">
+      <View
+        className="cash-coupon flex active"
+        style={{ backgroundImage: `url(${require("./active-bg.png")})` }}
+      >
+        <View
+          className="secondary flex center"
+          // style={{ backgroundImage: `url(${secondaryActiveBg})` }}
+        >
           <View className="money-wrap">
             ￥<Text className="money">50</Text>
             <View className="info">满199可用</View>
           </View>
-          <View className="bg flex">
-            <View className="mask item" />
-            <View className="pl">
-              <View
-                className="after"
-                style={{ background: this.props.background }}
-              />
-              <View
-                className="before"
-                style={{ background: this.props.background }}
-              />
-            </View>
-          </View>
         </View>
+        {/* <Image
+          className="middle-bg"
+          mode="widthFix"
+          src={require("./middle-bg.png")}
+        /> */}
         <View className="item content">
           <View className="head flex">
             <View className="label flex center">现金券</View>洛溪路店
           </View>
           <View className="date">2019.4.20-2019.4.30</View>
           <View className="info">极速退/免预约/全部商品可用</View>
-          <View className="bg flex">
-            <View className="pl">
-              <View
-                className="after"
-                style={{ background: this.props.background }}
-              />
-              <View
-                className="before"
-                style={{ background: this.props.background }}
-              />
-            </View>
-            <View className="mask item" />
-          </View>
         </View>
       </View>
     );
