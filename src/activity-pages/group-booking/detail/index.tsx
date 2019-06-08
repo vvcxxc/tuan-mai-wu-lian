a4eb3b8e0be8600d4588b2dd83c1a4f3e6e4e359
domen <mixiaofeng1996@gmail.com>
2019-06-09 00:01:04 +0800

    feat: 详情

--- src/activity-pages/group-booking/detail/index.tsx
+++ src/activity-pages/group-booking/detail/index.tsx
@@ -14,33 +14,53 @@ class GroupBookingDetail extends PureComponent {
 
 	render() {
 		return (
-			<View className="page">
-				<View className="head">
-					<Swiper>
-						<SwiperItem>
-							<Image className="banner-img" mode="widthFix" src={require('../../../assets/banner.png')} />
-						</SwiperItem>
-					</Swiper>
-					<View className="purchase-tip flex center left">
-						<Image src="" className="img" />
-						杨温饱 拼团成功
-					</View>
-				</View>
-				<View className="content">
-					<View className="title">一颗红包起司蛋糕餐券</View>
-					<View className="time">活动时间：2019.3.10-2019.4.10</View>
-					<View className="labels">
-						<View className="item">送价值3000元蓝牙耳机</View>
-						<View className="item">3人团</View>
-						<View className="item">24小时</View>
-					</View>
-					<View className="detail">
-						<View className="title">商品详情</View>
-						<View className="text">
-							简介详情简介详情简介详情简介详情简介详情简介详情简介
-							简介详情简介详情简介详情简介详情简介详情。
+			<View className="page flex column">
+				<View className="item">
+					<View className="head">
+						<Swiper>
+							<SwiperItem>
+								<Image
+									className="banner-img"
+									mode="widthFix"
+									src={require('../../../assets/banner.png')}
+								/>
+							</SwiperItem>
+						</Swiper>
+						<View className="purchase-tip flex center left">
+							<Image src="" className="img" />
+							杨温饱 拼团成功
 						</View>
 					</View>
+					<View className="content">
+						<View className="title">一颗红包起司蛋糕餐券</View>
+						<View className="time">活动时间：2019.3.10-2019.4.10</View>
+						<View className="labels">
+							<View className="item">送价值3000元蓝牙耳机</View>
+							<View className="item">3人团</View>
+							<View className="item">24小时</View>
+						</View>
+						<View className="detail">
+							<View className="title">商品详情</View>
+							<View className="text">
+								简介详情简介详情简介详情简介详情简介详情简介详情简介
+								简介详情简介详情简介详情简介详情简介详情。
+							</View>
+							<View className="see-more flex center">
+								查看更多
+								<Image src={require('./arrow-down.png')} mode="widthFix" className="img" />
+							</View>
+						</View>
+					</View>
+				</View>
+				<View className="footer flex">
+					<View className="price item flex">
+						<View className="current">
+							<View className="small">￥</View>100
+						</View>
+						<View className="old">¥300（含8元运费）</View>
+					</View>
+					<View className="btn pay">立即购买</View>
+					<View className="btn regiment">发起拼团</View>
 				</View>
 			</View>
 		);
