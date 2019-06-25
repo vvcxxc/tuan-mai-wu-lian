1bfae53b5e0d582c666ceb50cbf7bf039739e5cd
zhaolin <601996845@qq.com>
2019-06-25 17:45:24 +0800

    zl

--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -12,16 +12,33 @@ export default class Order extends Component {
   state = {
     current: 0,
     coupon: [],
+    currentShowList:[],
     page: 1,
     loading: false
   };
 
-  handlerTabChange(current) {
-    this.setState({ current },()=>{
-       console.log(current)
+  handlerTabChange(current:number) {
+    this.setState({
+       current ,
+       currentShowList : this.handerListType(current),
     });
   }
-
+  //判断四种优惠券的类型
+  handerListType=(type:number)=>{
+     const { coupon } = this.state
+     let now = Date.parse( String( new Date() ) )
+     switch(type){
+        case 0: //未使用 ---->当前时间早于过期时间 ， 且没有使用时间
+         return coupon.filter( (i:any)=> Date.parse( i.expiration ) > now && i.status * 1 === 1    )
+        case 1: //已使用 ---->有使用时间
+         return coupon.filter((i:any) => i.status * 1 === 2 )
+        case 2: //已过期 ---->当前时间晚于过期时间 ， 且没有使用时间
+         return coupon.filter((i:any) => now >  Date.parse( i.expiration ) && i.status * 1 === 1    )
+        case 3: //已退款
+         return coupon.filter((i:any) => i.status * 1 === 3 )
+        default : return []
+     }
+  }
   componentWillMount() {
     this.getData();
   }
@@ -32,18 +49,18 @@ export default class Order extends Component {
       url: "v3/user/coupons",
       data: { coupons_status: this.state.current + 1, page: this.state.page }
     })
-      .then((res: any) => this.setState({ coupon: res, loading: false }))
+      .then((res: any) => this.setState({ coupon: res.data, loading: false }))
       .catch(() => this.setState({ loading: false }));
   }
 
   render() {
-    const { current } = this.state
+    const { current , currentShowList } = this.state
     const list = ["未使用", "已使用", "已过期", "已退款"];
     return (
       <View className="order flex column">
         <Tabs list={list} onChange={this.handlerTabChange.bind(this)} />
         <ScrollView scrollY className="item content-wrap">
-          <Content   type={current}   list={[ { coupons_id:0 } ]} loading={this.state.loading} />
+          <Content   type={current}   list={currentShowList} loading={this.state.loading} />
         </ScrollView>
       </View>
     );
