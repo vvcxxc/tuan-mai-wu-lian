ddc3d0a0bd591a19f2007df1a0c0c662699cd556
vvcxxc <asd123>
2019-08-05 11:15:37 +0800

    定位

--- src/business-pages/set-meal/index.tsx
+++ src/business-pages/set-meal/index.tsx
@@ -48,7 +48,9 @@ export default class PaySuccess extends Component {
       sname: "",
       tel: "",
       distance: "",
-      shop_door_header_img:""
+      shop_door_header_img: "",
+      xpoint: 0,
+      ypoint: 0
     },
     goods_album: [
       {
@@ -155,6 +157,16 @@ export default class PaySuccess extends Component {
     //     // }
     //   })
   }
+  routePlanning = (e) => {
+    Taro.openLocation({
+      latitude: Number(this.state.store.ypoint),
+      longitude: Number(this.state.store.xpoint),
+      scale: 18,
+      name: this.state.store.sname,
+      address: this.state.store.saddress,
+    })
+    e.stopPropagation();
+  }
   render() {
     return (
       <View className="set-meal">
@@ -196,7 +208,7 @@ export default class PaySuccess extends Component {
           } */}
           <View className="tit">{this.state.coupon.yname} </View>
 
-          <View className="desc"  style={{height:"20px"}}></View>
+          <View className="desc" style={{ height: "20px" }}></View>
 
           <View className="tags">
             <Text className="tag-text" style={{ backgroundColor: this.state.coupon.label.indexOf('可叠加') !== -1 ? '#fde8e5' : '#fff' }}>可叠加</Text>
@@ -211,20 +223,19 @@ export default class PaySuccess extends Component {
           <View className="flex center">
             <Image className="image" src={this.state.store.shop_door_header_img} />
             <View className="item">
-              <View className="tit"  style={{fontWeight:"bold",fontSize:"16px"}}>{this.state.store.sname}</View>
+              <View className="tit" style={{ fontWeight: "bold", fontSize: "16px" }}>{this.state.store.sname}</View>
               <View className="money" ></View>
             </View>
             <AtIcon value="chevron-right" color="#999" size="24px" />
           </View>
           <View className="address-view flex center">
-            <View style={{ width: "10%" }}>
-              <Image className="address-image" src={AddressImg} />
-            </View>
-            <View className="distance">{this.state.store.distance}</View>
-            <View className="text flex-item" style={{ width: "80%" }}>{this.state.store.saddress}</View>
-            <View style={{ width: "10%" }}>
-              <Image className="mobile-image" src={MobileImg} onClick={this.makePhoneCall.bind(this)} />
-            </View>
+
+            <Image className="address-image" src={AddressImg} onClick={this.routePlanning.bind(this)}/>
+            <View className="distance" onClick={this.routePlanning.bind(this)}>{this.state.store.distance}</View>
+            <View className="text flex-item" style={{ width: "80%" }} onClick={this.routePlanning.bind(this)}>{this.state.store.saddress}</View>
+
+            <Image className="mobile-image" src={MobileImg} onClick={this.makePhoneCall.bind(this)} />
+
           </View>
         </View>
         <View className="remark mt20 pd30 bcff">
--- src/business-pages/ticket-buy/index.tsx
+++ src/business-pages/ticket-buy/index.tsx
@@ -48,7 +48,9 @@ export default class PaySuccess extends Component {
       sname: "",
       tel: "",
       distance: "",
-      shop_door_header_img:""
+      shop_door_header_img:"",
+      xpoint:0,
+      ypoint:0
     },
     goods_album: [
       {
@@ -154,7 +156,16 @@ export default class PaySuccess extends Component {
     //     // }
     //   })
   }
-
+  routePlanning = (e) => {
+		Taro.openLocation({
+			latitude: Number(this.state.store.ypoint),
+			longitude: Number(this.state.store.xpoint),
+			scale: 18,
+			name: this.state.store.sname,
+			address: this.state.store.saddress,
+		})
+		e.stopPropagation();
+	}
   render() {
     return (
       <View className="set-meal">
@@ -198,9 +209,9 @@ export default class PaySuccess extends Component {
             <AtIcon value="chevron-right" color="#999" size="24px" />
           </View>
           <View className="address-view flex center">
-            <Image className="address-image" src={AddressImg} />
-            <View className="distance">{this.state.store.distance}</View>
-            <View className="text flex-item">{this.state.store.saddress}</View>
+            <Image className="address-image" src={AddressImg} onClick={this.routePlanning.bind(this)}/>
+            <View className="distance" onClick = {this.routePlanning.bind(this)}>{this.state.store.distance}</View>
+            <View className="text flex-item" onClick = {this.routePlanning.bind(this)}>{this.state.store.saddress}</View>
             <Image className="mobile-image" src={MobileImg} onClick={this.makePhoneCall.bind(this)} />
           </View>
         </View>
