1fd673e3dfc94643ae1ece0907ce4730c0d8e3ba
vvcxxc <asd123>
2019-10-15 17:47:42 +0800

    删减

--- src/pages/activity/group/index.tsx
+++ src/pages/activity/group/index.tsx
@@ -78,8 +78,6 @@ export default class Group extends Component<Props>{
     is_login: false,
     isFromShare: false,
     groupListShow: false,
-    differ_time: [],
-    touthstartY: 0,
     groupListPages: 1
   };
 
@@ -135,7 +133,6 @@ export default class Group extends Component<Props>{
               }
               this.setState({ data: res.data }, () => {
                 this.draw();
-                this.tempTime();
               });
               Taro.hideLoading()
             }).catch(err => {
@@ -181,7 +178,6 @@ export default class Group extends Component<Props>{
               }
               this.setState({ data: res.data }, () => {
                 this.draw();
-                this.tempTime();
               });
               Taro.hideLoading()
             }).catch(err => {
@@ -529,24 +525,6 @@ export default class Group extends Component<Props>{
       })
     })
     e.stopPropagation();
-  }
-
-  tempTime = () => {
-    let temp_Time = new Date(this.state.data.end_time).getTime() - new Date().getTime();   //时间差的毫秒数        
-    //计算出相差天数  
-    var days = Math.floor(temp_Time / (24 * 3600 * 1000))
-    //计算出小时数  
-    var leave1 = temp_Time % (24 * 3600 * 1000)    //计算天数后剩余的毫秒数  
-    var hours = Math.floor(leave1 / (3600 * 1000))
-    console.log('小时', days, hours)
-    //计算相差分钟数  
-    var leave2 = leave1 % (3600 * 1000)        //计算小时数后剩余的毫秒数  
-    var minutes = Math.floor(leave2 / (60 * 1000))
-    //计算相差秒数  
-    var leave3 = leave2 % (60 * 1000)      //计算分钟数后剩余的毫秒数  
-    var seconds = Math.round(leave3 / 1000)
-    var differ_time = [days, hours, minutes, seconds]
-    this.setState({ differ_time: differ_time });
   }
 
   addGroupList = () => {
