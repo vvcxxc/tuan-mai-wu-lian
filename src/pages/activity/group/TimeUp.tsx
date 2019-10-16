a1eb770cffdf222cb238e0e00446e368b2c42cd7
vvcxxc <asd123>
2019-10-16 10:31:35 +0800

    清除计时器

--- src/pages/activity/group/TimeUp.tsx
+++ src/pages/activity/group/TimeUp.tsx
@@ -27,7 +27,13 @@ export default class TimeUp extends Component<Props>{
         this.setTime();
     }
     componentWillUnmount() {
-        clearTimeout(timer);
+        console.log('清除计时器');
+        // clearTimeout(timer);
+        var end = setTimeout(function () { }, 1);
+        var start = (end - 100) > 0 ? end - 100 : 0;
+        for (var i = start; i <= end; i++) {
+            clearTimeout(i);
+        }
     }
     /**
        * 定时
