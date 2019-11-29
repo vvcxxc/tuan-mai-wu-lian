01c422da1dd5d8719de46895d4980295f3b9141b
vvcxxc <asd123>
2019-11-29 17:59:32 +0800

    提取请求

--- src/pages/business/index.tsx
+++ src/pages/business/index.tsx
@@ -156,8 +156,8 @@ export default class PaySuccess extends Component<Props> {
       }
     })
   }
-  
-  requestData() {
+
+  requestData = () => {
     Taro.showLoading({
       title: 'loading',
     })
@@ -195,14 +195,14 @@ export default class PaySuccess extends Component<Props> {
   }
 
   //去拼团活动
-  gotoGroup(_id, gift_id, activity_id) {
+  gotoGroup = (_id, gift_id, activity_id) => {
     Taro.navigateTo({
       url: '/pages/activity/group/index?id=' + _id + '&type=5&gift_id=' + gift_id + '&activity_id=' + activity_id
       // url: '/pages/activity/pages/detail/detail?id=' + _id + '&type=5&gift_id=' + gift_id + '&activity_id=' + activity_id
     })
   }
   // 去增值活动
-  gotoAppreciation(_id, gift_id, activity_id) {
+  gotoAppreciation = (_id, gift_id, activity_id) => {
 
     Taro.navigateTo({
       url: '/pages/activity/appreciation/index?id=' + _id + '&type=1&gift_id=' + gift_id + '&activity_id=' + activity_id
