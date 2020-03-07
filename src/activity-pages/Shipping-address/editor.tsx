1691b97a0e4369eaed6624fa851ed811eb51d2b1
vvcxxc <asd123>
2020-03-07 08:44:31 +0800

    fix:城市id

--- src/activity-pages/Shipping-address/editor.tsx
+++ src/activity-pages/Shipping-address/editor.tsx
@@ -80,7 +80,7 @@ export default class EditorAddress extends Component {
     }
     // 所在区域
     cityEnd = (query) => {
-        this.setState({ cityValue: query })
+        this.setState({ cityValue: query.tempselectorid })
         // this.setState({ cityValue: query.tempselectorid, tempCityInfo: query.selectorChecked, actionsheetShow: false })
     }
     //详细地址
