b7d4960228462094f70fdbd8316b1dffbba7fc26
songyizhe <769724661@qq.com>
2020-03-08 14:11:20 +0800

    fix: 测试环境

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
     NODE_ENV: '"production"',
-    BASIC_API: '"https://api.tdianyi.com/"',
+    // BASIC_API: '"https://api.tdianyi.com/"',
     USER_API: '"http://test.usercenter.tdianyi.com/"',
-    // BASIC_API: '"https://test.api.tdianyi.com/"'
+    BASIC_API: '"https://test.api.tdianyi.com/"'
     // BASIC_API: '"https://release.api.tdianyi.com/"'
   },
   defineConstants: {
