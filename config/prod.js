7526fdaaac5964665cd5c98070441983834b72ba
songyizhe <769724661@qq.com>
2020-03-09 13:44:47 +0800

    fix:

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     NODE_ENV: '"production"',
     // BASIC_API: '"https://api.tdianyi.com/"',
-    USER_API: '"http://test.usercenter.tdianyi.com/"',
+    USER_API: '"https://test.usercenter.tdianyi.com/"',
     BASIC_API: '"https://test.api.tdianyi.com/"'
     // BASIC_API: '"https://release.api.tdianyi.com/"'
   },
