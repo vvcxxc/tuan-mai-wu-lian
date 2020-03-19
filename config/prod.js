95a37cfa1b491b928c6bbc0e57706455e1d24328
songyizhe <769724661@qq.com>
2020-03-19 11:21:13 +0800

    chore: 环境修改

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,10 @@
 module.exports = {
   env: {
     NODE_ENV: '"production"',
-    BASIC_API: '"https://api.tdianyi.com/"',
-    USER_API: '"https://usercenter.tdianyi.com/"',
-    // USER_API: '"https://test.usercenter.tdianyi.com/"',
-    // BASIC_API: '"https://test.api.tdianyi.com/"'
+    // BASIC_API: '"https://api.tdianyi.com/"',
+    // USER_API: '"https://usercenter.tdianyi.com/"',
+    USER_API: '"https://test.usercenter.tdianyi.com/"',
+    BASIC_API: '"https://test.api.tdianyi.com/"'
     // USER_API: '"http://release.usercenter.tdianyi.com/"',
     // BASIC_API: '"http://release.api.tdianyi.com/"'
   },
