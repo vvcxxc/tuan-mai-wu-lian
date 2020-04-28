8c5f724a967d9c4597405e61a6eccfe66aa5e4bf
songyizhe <769724661@qq.com>
2020-04-28 19:20:23 +0800

    fix: 分享和邀请店铺链接

--- config/prod.js
+++ config/prod.js
@@ -4,12 +4,13 @@ module.exports = {
     // BASIC_API: '"https://api.tdianyi.com/"',
     // USER_API: '"https://usercenter.tdianyi.com/"',
     // H5_URL: 'http://mall.tdianyi.com',
+    // SUPPLIER_URL: 'http://supplier.tdianyi.com/login/register',
     USER_API: '"https://test.usercenter.tdianyi.com/"',
     BASIC_API: '"https://test.api.tdianyi.com/"',
     H5_URL: 'http://test.mall.tdianyi.com',
+    SUPPLIER_URL: 'http://test.supplierv2.tdianyi.com/login/register',
     // USER_API: '"http://release.usercenter.tdianyi.com/"',
     // BASIC_API: '"http://release.api.tdianyi.com/"'
-    SUPPLIER_URL: 'http://supplier.tdianyi.com/login/register'
   },
   defineConstants: {
   },
