869732be4f5464a22353b901c725bd5be54e232e
songyizhe <769724661@qq.com>
2020-05-12 17:51:43 +0800

    misc

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
     NODE_ENV: '"development"',
-    // BASIC_API: '"https://api.tdianyi.com/"',
-    // USER_API: '"https://usercenter.tdianyi.com/"',
-    // H5_URL: 'http://mall.tdianyi.com',
-    // SUPPLIER_URL: 'http://supplier.tdianyi.com/login/register',
-    USER_API: '"https://test.usercenter.tdianyi.com/"',
-    BASIC_API: '"https://test.api.tdianyi.com/"',
-    H5_URL: 'http://test.mall.tdianyi.com',
-    SUPPLIER_URL: 'http://test.supplierv2.tdianyi.com/login/register',
+    BASIC_API: '"https://api.tdianyi.com/"',
+    USER_API: '"https://usercenter.tdianyi.com/"',
+    H5_URL: 'http://mall.tdianyi.com',
+    SUPPLIER_URL: 'http://supplier.tdianyi.com/login/register',
+    // USER_API: '"https://test.usercenter.tdianyi.com/"',
+    // BASIC_API: '"https://test.api.tdianyi.com/"',
+    // H5_URL: 'http://test.mall.tdianyi.com',
+    // SUPPLIER_URL: 'http://test.supplierv2.tdianyi.com/login/register',
     // SUPPLIER_URL: 'http://release.supplierv2.tdianyi.com/login/register',
     // USER_API: '"http://release.usercenter.tdianyi.com/"',
     // BASIC_API: '"http://release.api.tdianyi.com/"'
